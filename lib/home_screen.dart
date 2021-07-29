4a6f26ff188614aa01eadd5f0360d185bb494df5
RGTechPro <73791635+RGTechPro@users.noreply.github.com>
2021-07-29 19:50:15 +0530

    added search bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,6 +8,31 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            height: 45,
+            child: TextField(
+                textInputAction: TextInputAction.search,
+                onSubmitted: (value) {},
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.blue,
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.blue)),
+                  hintText: 'Search Crypto',
+                )),
+          ),
+        ),
+      ],
+    ));
   }
 }
